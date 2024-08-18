0c1ad3bf5abf9c75db99695c1f4cc245d4769f96
Babu Nair <babunairofficial@gmail.com>
2024-08-18 19:42:44 +0530

    fashion store - footer

--- FashionStoreVue/src/App.vue
+++ FashionStoreVue/src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <Header />    
+    <Header />
+    
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header, 
+    Footer
     
   }
 }
---
+++ FashionStoreVue/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <footer>&#169; 2012-2023 FashionStore.com</footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Footer'
+  }
+  </script>
