4e07856c41c121c19f000d59c5f61ab904a9795a
Babu Nair <babunairofficial@gmail.com>
2024-08-19 10:27:16 +0530

    fashion store - ShoppingBanner

--- FashionStoreVue/src/App.vue
+++ FashionStoreVue/src/App.vue
@@ -4,6 +4,7 @@
 
     <main>
       <ItemsGrid />
+      <ShoppingBanner />
     </main>
     
     <Footer/>
@@ -14,6 +15,7 @@
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 import ItemsGrid from './components/ItemsGrid.vue'
+import ShoppingBanner from './components/ShoppingBanner.vue';
 
 
 
@@ -21,7 +23,8 @@ export default {
   name: 'App',
   components: {
     Header,
-    ItemsGrid, 
+    ItemsGrid,
+    ShoppingBanner, 
     Footer
     
   }
---
+++ FashionStoreVue/src/components/ShoppingBanner.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="Shopping-container">
+      <a href="">
+        <img src="https://via.placeholder.com/150?text=Shopping+Time" alt="shopping time" id="shopping-time-img">
+      </a>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ShoppingBanner'
+  }
+  </script>
