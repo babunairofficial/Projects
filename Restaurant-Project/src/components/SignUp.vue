ce5b18dccdd4bbd307b43558800fc5ba78630de3
Babu Nair <babunairofficial@gmail.com>
2024-08-25 11:29:38 +0530

    Restaurant - local storage lock for signup

--- Restaurant-Project/src/components/SignUp.vue
+++ Restaurant-Project/src/components/SignUp.vue
@@ -38,6 +38,12 @@ import axios from 'axios';
                     this.$router.push({name:"Home"})
                 }
             }
+        },
+        mounted(){
+            let user = localStorage.getItem('user-info');
+            if(user){
+                this.$router.push({name:'Home'})
+            }
         }
     }
 </script>
