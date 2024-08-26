d170e1534ce53770f4eaea64229dc9d1f9d6e4d7
Babu Nair <babunairofficial@gmail.com>
2024-08-26 08:57:14 +0530

    Restaurant - Header

--- Restaurant-Project/src/components/Home.vue
+++ Restaurant-Project/src/components/Home.vue
@@ -1,10 +1,17 @@
 <template>
+    <Header />
     <h1>Hello User, Welcome on Home Page</h1>
 </template>
 
 <script>
+import Header from './Header.vue';
 export default{
     name:"Home",
+
+    components:{
+        Header
+    },
+
     mounted(){
             let user = localStorage.getItem('user-info');
             if(!user){
