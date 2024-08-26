43bdd18846ebee46b10254c66ecec2e07291c92a
Babu Nair <babunairofficial@gmail.com>
2024-08-26 08:47:42 +0530

    Restaurant - login page functionality

--- Restaurant-Project/src/components/Login.vue
+++ Restaurant-Project/src/components/Login.vue
@@ -4,7 +4,7 @@
     <div class="login">        
         <input type="text" v-model="email" placeholder="Enter Email" />
         <input type="password" v-model="password" placeholder="Enter Password" />
-        <button v-on:click="signUp"> Login</button>
+        <button v-on:click="login"> Login</button>
         <p>
             <router-link to="/sign-up">Sign Up</router-link>
         </p>
@@ -12,7 +12,35 @@
 </template>
 
 <script>
+import axios  from 'axios';
 export default{
-    name:"Login"
+    name:"Login",
+    data(){
+        return{
+            email:'',
+            password: ''
+        }
+    },
+    methods:{
+        async login(){
+            let result = await axios.get(
+                `http://localhost:3000/users?email=${this.email}&password=${this.password}`
+            )
+            console.log(result);
+            if(result.status==200 && result.data.length>0){
+                    alert("logged in successfully");
+                    localStorage.setItem("user-info",JSON.stringify(result.data[0]))
+                    this.$router.push({name:"Home"})
+                }
+        }
+
+    },
+    mounted(){
+            let user = localStorage.getItem('user-info');
+            if(user){
+                this.$router.push({name:'Home'})
+            }
+        }
+
 }
 </script>
