56819c57204831d36533d60c6fa81a3747b1929f
Babu Nair <babunairofficial@gmail.com>
2024-08-27 20:15:38 +0530

    Restaurant - restaurant list display using api

--- Restaurant-Project/src/components/Home.vue
+++ Restaurant-Project/src/components/Home.vue
@@ -1,15 +1,31 @@
 <template>
     <Header />
     <h1>Hello {{name}}, Welcome on Home Page</h1>
+    <table border="1">
+        <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Contact</td>
+            <td>Address</td>
+        </tr>
+        <tr v-for="item in restaurant" :key="item.id">
+            <td>{{ item.id }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.contact }}</td>
+            <td>{{ item.address }}</td>
+        </tr>
+    </table>
 </template>
 
 <script>
 import Header from './Header.vue';
+import axios from 'axios';
 export default{
     name:"Home",
     data(){
         return{
-            name:''
+            name:'',
+            restaurant:[]
         }
     },
 
@@ -17,12 +33,22 @@ export default{
         Header
     },
 
-    mounted(){
+    async mounted(){
             let user = localStorage.getItem('user-info');
             this.name = JSON.parse(user).name;
             if(!user){
                 this.$router.push({name:'SignUp'})
             }
+            let result = await axios.get("http://localhost:3000/restaurants");
+            console.warn(result);
+            this.restaurant=result.data
+
         }
 }
 </script>
+<style>
+td{
+    width: 160px;
+    height: 40px;
+}
+</style>
