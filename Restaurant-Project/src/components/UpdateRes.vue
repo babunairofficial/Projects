2f4b0ff45bce9e32901ff769cbb534cf325d873d
Babu Nair <babunairofficial@gmail.com>
2024-08-30 22:43:44 +0530

    Restaurant - update restaurant

--- Restaurant-Project/src/components/UpdateRes.vue
+++ Restaurant-Project/src/components/UpdateRes.vue
@@ -1,6 +1,12 @@
 <template>
     <Header />
     <h1>Hello User, Welcome on Update Restaurant Page</h1>
+    <form class="add">
+        <input type="text" name="name" placeholder="Enter Name" v-model="Restaurant.name"/>
+        <input type="text" name="address" placeholder="Enter Address" v-model="Restaurant.address"/>
+        <input type="text" name="contact" placeholder="Enter Contact" v-model="Restaurant.contact"/>
+        <button type="button" v-on:click="addRestaurant">Update Restaurant</button>
+    </form>
 </template>
 
 <script>
@@ -10,6 +16,16 @@ export default{
 
     components:{
         Header
+    },
+    data(){
+        return{
+            Restaurant:{
+                name:'',
+                address:'',
+                contact:''
+            }
+        }
+
     },
 
     mounted(){
