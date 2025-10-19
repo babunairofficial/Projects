893a146528804e03d6a2363c3a8c54f03f86b658
Babu Nair <babunairofficial@gmail.com>
2025-10-19 20:04:50 +0530

    Major Project - new & create route

--- MajorProject/app.js
+++ MajorProject/app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
 
+
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderloft";
 
 main().then(() => {
@@ -21,6 +22,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({extended: true}));
 
+
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
@@ -32,12 +34,27 @@ app.get("/listings", async (req, res) => {
     res.render("listings/index.ejs", {allListings});
 });
 
+//New Route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+
 //Show Route
 app.get("/listings/:id", async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing });
 });
+
+//Create Route
+app.post("/listings", async (req, res) => {
+    // let {title, description, image, price, country, location} = req.body;
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
