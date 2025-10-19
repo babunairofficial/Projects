fe9a7635640a26e4c0e79d41b80dce2e65d187f5
Babu Nair <babunairofficial@gmail.com>
2025-10-19 09:35:35 +0530

    Major Project - index route

--- MajorProject/app.js
+++ MajorProject/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("../models/listing");
+const Listing = require("./models/listing");
+const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderloft";
 
@@ -15,23 +16,34 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
 
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "My Villa",
-        description: "By the beach",
-        price: 2500,
-        location: "Calangute, Goa",
-        country: "India",
-    });
 
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successful testing");
+//index route
+app.get("/listings", async (req, res) => {
+   const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
 });
+
+
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "My Villa",
+//         description: "By the beach",
+//         price: 2500,
+//         location: "Calangute, Goa",
+//         country: "India",
+//     });
+
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful testing");
+// });
 
 
 app.listen(8080, () => {
