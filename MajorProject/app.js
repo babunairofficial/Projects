7fb65e471fdd088d55c066c5e5f6b855ec160789
Babu Nair <babunairofficial@gmail.com>
2025-10-19 12:26:49 +0530

    Major Project - show route

--- MajorProject/app.js
+++ MajorProject/app.js
@@ -19,6 +19,8 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
@@ -30,6 +32,12 @@ app.get("/listings", async (req, res) => {
     res.render("listings/index.ejs", {allListings});
 });
 
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+});
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
