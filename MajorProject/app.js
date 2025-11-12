beee44ffe979b22229783dfd9a6ebdeb301a0699
Babu Nair <babunairofficial@gmail.com>
2025-11-12 22:57:16 +0530

    Major Project - custom error handling

--- MajorProject/app.js
+++ MajorProject/app.js
@@ -56,10 +56,15 @@ app.get("/listings/:id", async (req, res) => {
 
 //Create Route
 app.post("/listings", async (req, res) => {
-    // let {title, description, image, price, country, location} = req.body;
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
+    try {
+        // let {title, description, image, price, country, location} = req.body;
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings");
+    } catch(err) {
+        next(err);
+    }
+    
 });
 
 //Edit Route
@@ -98,6 +103,9 @@ app.delete("/listings/:id", async (req, res) => {
 //     res.send("successful testing");
 // });
 
+app.use((err, req, res, next) => {
+    res.send("something went wrong!");
+});
 
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
