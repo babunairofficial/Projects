b7e46cc747c14a24982e2278c72b4e69a99f779f
Babu Nair <babunairofficial@gmail.com>
2025-11-13 22:30:56 +0530

    Major Project - custom wrapAsync

--- MajorProject/app.js
+++ MajorProject/app.js
@@ -5,6 +5,8 @@ const Listing = require("./models/listing");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync");
+
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderloft";
@@ -55,17 +57,13 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 //Create Route
-app.post("/listings", async (req, res) => {
-    try {
+app.post("/listings", wrapAsync( async (req, res) => {
         // let {title, description, image, price, country, location} = req.body;
         const newListing = new Listing(req.body.listing);
         await newListing.save();
         res.redirect("/listings");
-    } catch(err) {
-        next(err);
-    }
-    
-});
+    })
+);
 
 //Edit Route
 app.get("/listings/:id/edit", async (req, res) =>{
